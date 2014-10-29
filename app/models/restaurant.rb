41022cd6f3fa9adc325a53f500d9a02a3ad5ab31
Andy Weiss <andyweiss1982@gmail.com>
2014-10-29 16:27:20 -0400

    restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true, numericality: true, length: { is: 5 }
+  validates :phone, presence: true, numericality: true, length: { is: 10 }
 end
