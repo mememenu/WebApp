e6344b919ae9ddc9de2ad3b29eba4350c4b99822
Andy Weiss <andyweiss1982@gmail.com>
2014-10-29 10:25:32 -0400

    additional user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe User, :type => :model do
     user.save
     expect(User.count).to eq(1)
   end
+
+  it "should not save a user if the password is less that 8 characters" do 
+    expect(User.count).to eq(0)
+    user = User.new
+    user.email = "alfonsopintos@gmail.com"
+    user.password = "1234567"
+    user.password_confirmation = "1234567"
+    user.save
+    expect(User.count).to eq(0)
+  end
+
+    it "should not save a user if the email is already taken" do 
+    expect(User.count).to eq(0)
+    user = User.new
+    user.email = "alfonsopintos@gmail.com"
+    user.password = "12345678"
+    user.password_confirmation = "12345678"
+    user.save
+    expect(User.count).to eq(1)
+    duplicate_user = User.new
+    duplicate_user.email = "alfonsopintos@gmail.com"
+    duplicate_user.password = "12345678"
+    duplicate_user.password_confirmation = "12345678"
+    duplicate_user.save
+    expect(User.count).to eq(1)
+  end
+
+
+
+
 end
