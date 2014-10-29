2a2ac08439dc0cd41b7c116bfe5bc1968b7e09de
Andy Weiss <andyweiss1982@gmail.com>
2014-10-29 16:50:41 -0400

    restaurant unit tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   validates :name, presence: true
   validates :address_1, presence: true
   validates :city, presence: true
-  validates :state, presence: true
+  validates :state, presence: true, length: { is: 2}
   validates :zipcode, presence: true, numericality: true, length: { is: 5 }
   validates :phone, presence: true, numericality: true, length: { is: 10 }
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should save a restaurant if all validations pass" do
+    expect(Restaurant.count).to eq(0)
+    rest = Restaurant.new
+    rest.name = "mmmm"
+    rest.address_1 = "123 fake lane"
+    rest.address_2 = "apartment 4"
+    rest.city = "miami"
+    rest.state = "fl"
+    rest.zipcode = "33134"
+    rest.phone = "3059999999"
+    rest.description = "tasty treats for you"
+    rest.dollars = 4
+    rest.reservations = true
+    rest.save
+    expect(Restaurant.count).to eq(1)
+  end
+
+
 end
