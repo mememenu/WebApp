a5045071a010476d48909a1ad465a50f2817e85c
alfonsopintos <alfonsopintos@gmail.com>
2014-11-01 13:07:40 -0400

    cuisine testing

--- spec/models/cuisine_spec.rb
+++ spec/models/cuisine_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Cuisine, :type => :model do
     cui.save
     expect(Cuisine.count).to eq(1)
   end
+
+  it "should not save if genre presence is not true" do
+    expect(Cuisine.count).to eq(0)
+    cui = Cuisine.new
+    cui.genre = nil
+    cui.save
+    expect(Cuisine.count).to eq(0)
+    expect { raise StandardError }.to raise_error
+  end
 end
