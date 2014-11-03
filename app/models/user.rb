dd719155d50de6fe17838ce59612a5a9141930a3
alfonsopintos <alfonsopintos@gmail.com>
2014-11-03 10:21:39 -0500

    omniauth callback now captures email from fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.email
+      user.email = auth.info.email
     end
   end
 
