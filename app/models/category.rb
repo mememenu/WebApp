eba4e809f390d6d1eeb5e57daa16486227753b5d
Andy Weiss <andyweiss1982@gmail.com>
2014-11-03 14:31:59 -0500

    removed display name validation from category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ActiveRecord::Base
+  has_many :dishes, through: :dish_categories
+  has_many :dish_categories, dependent: :destroy
+
+  validates :name, presence: true
+
+
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,11 @@
 class Dish < ActiveRecord::Base
   belongs_to :menu
+
+  has_many :categories, through: :dish_categories
+  has_many :dish_categories, dependent: :destroy
+
+  validates :name, presence: true
+  validates :portion_size, presence: true
+  validates :spice, presence: true
+
 end
