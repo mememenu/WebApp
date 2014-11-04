b52f051b51d6fa325c5405f54badaa695098a5c3
Andy Weiss <andyweiss1982@gmail.com>
2014-11-04 13:31:09 -0500

    remove dsiplay name validation from menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,6 @@
 class Menu < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :display_name, presence: true
 
   has_many :restaurants, through: :restaurant_menus
   has_many :restaurant_menus, dependent: :destroy
