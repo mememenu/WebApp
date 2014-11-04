7bf380fa3fa1e03efc00c82b02209e46f7feb288
alfonsopintos <alfonsopintos@gmail.com>
2014-11-04 15:58:53 -0500

    added avatar to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,6 +13,10 @@ class Restaurant < ActiveRecord::Base
 
   has_many :menus, through: :restaurant_menus
   has_many :restaurant_menus, dependent: :destroy
+
+  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104185108) do
+ActiveRecord::Schema.define(version: 20141104205556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,10 @@ ActiveRecord::Schema.define(version: 20141104185108) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "hide"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
