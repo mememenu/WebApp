a73deb66f651eb038e960a3483a211c2f7eb9b03
alfonsopintos <alfonsopintos@gmail.com>
2014-11-04 15:01:51 -0500

    fixed validation bugs

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,6 +5,7 @@ class Dish < ActiveRecord::Base
   validates :name, presence: true
   validates :portion_size, presence: true
   validates :spice, presence: true
+  validates :category_id, presence: true
 
   has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
