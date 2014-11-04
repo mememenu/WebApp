3665229dd75dd6f6745eb19d5a666166f2c34751
alfonsopintos <alfonsopintos@gmail.com>
2014-11-04 16:34:40 -0500

    landing page logos

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,7 +14,7 @@ class Restaurant < ActiveRecord::Base
   has_many :menus, through: :restaurant_menus
   has_many :restaurant_menus, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/placeholder_image1-1050x663.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
