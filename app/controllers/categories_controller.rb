e9578591549265e8f4d5c3c538505e44e48ac1be
alfonsopintos <alfonsopintos@gmail.com>
2014-11-04 10:35:56 -0500

    cleaned up schema, add dish from category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @dishes = Dish.where(:category_id, )
   end
 
   # GET /categories/new
