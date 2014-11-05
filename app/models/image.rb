bc4a05cd449903358d39f052c71d8fbe7fdd4d44
Andy Weiss <andyweiss1982@gmail.com>
2014-11-05 09:40:15 -0500

    image rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :dish, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "375x375>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
