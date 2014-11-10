c293806ae63f553ef72864de611d6800481835c8
alfonsopintos <alfonsopintos@gmail.com>
2014-11-10 15:22:43 -0500

    category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do 
+    @menu = Menu.create(name: "Lunch")
+  end
+
+  it "should save succesfully with name and menu_id" do
+    expect(Category.count).to eq(0)
+    @category = Category.new
+    @category.name = "Appetizer"
+    @category.menu_id = @menu.id
+    @category.save
+    expect(Category.count).to eq(1)
+  end
+
+  it "should not save if name is not present" do 
+    expect(Category.count).to eq(0)
+    @category = Category.new
+    @category.menu_id = @menu.id
+    @category.save
+    expect(Category.count).to eq(0)
+  end
+
+  it "should not save if menu_id is not present" do
+    expect(Category.count).to eq(0)
+    @category = Category.new
+    @category.name = "Appetizer"
+    @category.save
+    expect(Category.count).to eq(0)
+  end
+
 end
