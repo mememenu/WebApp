a3135ba7f666394617365eb6f983c5fb335c0d81
alfonsopintos <alfonsopintos@gmail.com>
2014-11-10 14:42:52 -0500

    restaurant menu specs

--- spec/models/restaurant_menu_spec.rb
+++ spec/models/restaurant_menu_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe RestaurantMenu, :type => :model do
     @menu = Menu.create(name: "Lunch")
   end
 
+  def create_restaurant_menu
+    @restaurant_menu = RestaurantMenu.new
+    @restaurant_menu.menu_id = @menu.id
+    @restaurant_menu.restaurant_id = @restaurant.id
+    @restaurant_menu.save
+  end
+
   it "should not save if restaurant_id is not present" do
     expect(RestaurantMenu.count).to eq(0)   
     @restaurant_menu = RestaurantMenu.new
@@ -23,14 +30,24 @@ RSpec.describe RestaurantMenu, :type => :model do
     expect(RestaurantMenu.count).to eq(0)
   end
 
-
-  it "should save if restaurant_menu has restaurant and menu associated with it" do
+  it "should save if restaurant_menu instance has restaurant and menu associated with it" do
     expect(RestaurantMenu.count).to eq(0)   
-    @restaurant_menu = RestaurantMenu.new
-    @restaurant_menu.menu_id = @menu.id
-    @restaurant_menu.restaurant_id = @restaurant.id
-    @restaurant_menu.save
+    create_restaurant_menu
     expect(RestaurantMenu.count).to eq(1)
+  end
+
+  it "should destroy restaurant_menu instance if dependent restaurant is destroyed" do
+    create_restaurant_menu
+    @restaurant.destroy
+    expect(RestaurantMenu.count).to eq(0)
+    expect(Restaurant.count).to eq(0)
+  end
+
+  it "should destroy restaurant_menu if dependent menu is destroyed" do
+    create_restaurant_menu
+    @menu.destroy
+    expect(RestaurantMenu.count).to eq(0)
+    expect(Menu.count).to eq(0)
   end
 
 end
