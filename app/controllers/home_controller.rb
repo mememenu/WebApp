9ea7960511b86ae3919f9305c68212694e41fc4f
Andy Weiss <andyweiss1982@gmail.com>
2014-11-13 14:58:24 -0500

    navbar minus dynamic restarants

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
+  get 'home/about'
+  get 'home/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
