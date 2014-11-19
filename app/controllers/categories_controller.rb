b148357e7520448e103562cf75def4c99124c494
alfonsopintos <alfonsopintos@gmail.com>
2014-11-19 10:04:46 -0500

    adding and editing category from restaurant show page working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -37,7 +37,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to @category.restaurant, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -51,7 +51,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to @category.restaurant, notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
