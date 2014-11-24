7050dd4ea7811599b17f8c28e5e78e36c515bf8d
alfonsopintos <alfonsopintos@gmail.com>
2014-11-24 13:27:02 -0500

    added mailer to production environemnt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'hello@meme.menu',
+    password: 'meme@2014'
+  }
 end
