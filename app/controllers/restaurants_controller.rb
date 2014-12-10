bb1503623597a2074c6991338249e822b2e1c3c3
alfonsopintos <alfonsopintos@gmail.com>
2014-12-10 11:33:45 -0500

    changed controller name to singular

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :ios]
-  before_action :validate_restaurant_owner_or_admin, except: [:show, :ios]
+  before_action :authenticate_user!, except: [:show]
+  before_action :validate_restaurant_owner_or_admin, except: [:show]
   before_action :check_hidden, only: :show
 
   # GET /restaurants
