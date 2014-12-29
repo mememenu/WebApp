102737c8c4410751f7746771a93791cc533c0106
alfonsopintos <alfonsopintos@gmail.com>
2014-12-29 17:39:46 -0500

    more fixing on restaurant tile paperclip size

--- app/models/restaurant_tile.rb
+++ app/models/restaurant_tile.rb
@@ -1,7 +1,7 @@
 class RestaurantTile < ActiveRecord::Base
   belongs_to :restaurant
 
-  has_attached_file :avatar, :styles => { :large => "648x648!"}, :default_url => "/images/placeholder_image1-1050x663.png"
+  has_attached_file :avatar, :styles => { :large => "648x648#"}, :default_url => "/images/placeholder_image1-1050x663.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
