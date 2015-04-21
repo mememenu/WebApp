d4743a1cdc8e2786e72e5333807414c96ba59395
alfonsopintos <alfonsopintos@gmail.com>
2015-04-21 15:42:38 -0400

    modified from an after_save to before_save callback for dish cloud front

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -15,7 +15,7 @@ class Dish < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 
-  after_save :create_cloudfront_url
+  before_save :create_cloudfront_url
   after_save :cascade_hidden, :if => :hide_changed?
   
 
