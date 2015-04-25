81415c3528591f567c765e2ad7976f5b35239d4b
alfonsopintos <alfonsopintos@gmail.com>
2015-04-25 13:03:47 -0400

    added menu id to feed endpoint

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,6 +1,6 @@
 class DishSerializer < ActiveModel::Serializer
   
-  attributes :id, :name, :description, :avatar, :restaurant_name, :restaurant_avatar, :restaurant_zone, :restaurant_city, :restaurant_region, :menu_name, :category_name, :monday_hours, :tuesday_hours, :wednesday_hours, :thursday_hours, :friday_hours, :saturday_hours, :sunday_hours, :website, :photographer_name, :photographer_media_link, :facebook, :twitter, :instagram, :cloud_front, :maps_url, :address_1, :state, :zipcode, :phone, :slug, :restaurant_header, :city
+  attributes :id, :name, :description, :avatar, :restaurant_name, :restaurant_avatar, :restaurant_zone, :restaurant_city, :restaurant_region, :menu_name, :category_name, :monday_hours, :tuesday_hours, :wednesday_hours, :thursday_hours, :friday_hours, :saturday_hours, :sunday_hours, :website, :photographer_name, :photographer_media_link, :facebook, :twitter, :instagram, :cloud_front, :maps_url, :address_1, :state, :zipcode, :phone, :slug, :restaurant_header, :city, :menu_id
 
   def restaurant_name
     object.restaurant.name
@@ -116,6 +116,10 @@ class DishSerializer < ActiveModel::Serializer
   
   def city
     object.restaurant.city
+  end
+
+  def menu_id
+    object.menu.id
   end
 
 end
