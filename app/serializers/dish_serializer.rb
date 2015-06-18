bd0c5c38ca50c102d7a2a316d9403e786af0b743
Braulio Martinez <braulio@paragon-labs.com>
2015-06-18 12:31:42 -0300

    Change exists? for present?.

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -117,12 +117,12 @@ class DishSerializer < ActiveModel::Serializer
   end
 
   def restaurant_header
-    object.restaurant.restaurant_header.avatar.exists? &&
+    object.restaurant.restaurant_header.avatar.present? &&
       object.restaurant.restaurant_header.avatar
   end
 
   def restaurant_banner
-    object.restaurant.restaurant_banner.avatar.exists? &&
+    object.restaurant.restaurant_banner.avatar.present? &&
       object.restaurant.restaurant_banner.avatar
   end
 
