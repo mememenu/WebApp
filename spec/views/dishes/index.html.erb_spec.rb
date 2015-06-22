3a0d7b9c99f63aea6f1628e11fb9b4d5d959d9d4
Braulio Martinez <braulio@paragon-labs.com>
2015-06-22 12:13:41 -0300

    Fix spec/views/dishes/index.html.erb_spec.rb

--- spec/views/dishes/index.html.erb_spec.rb
+++ spec/views/dishes/index.html.erb_spec.rb
@@ -1,46 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "dishes/index", :type => :view do
+RSpec.describe 'dishes/index', :type => :view do
+  let(:restaurant) { FactoryGirl.create(:restaurant, name: 'Parrillada') }
+  let(:dish1) { FactoryGirl.create(:dish, name: 'Asado', restaurant: restaurant) }
+  let(:dish2) { FactoryGirl.create(:dish, name: 'Chivito', restaurant: restaurant) }
+  let(:dishes) { [dish1, dish2] }
+
   before(:each) do
-    assign(:dishes, [
-      Dish.create!(
-        :name => "Name",
-        :description => "MyText",
-        :category => "Category",
-        :category_display => "Category Display",
-        :portion_size => "Portion Size",
-        :spice => 1,
-        :hot => false,
-        :gluten_free => false,
-        :vegetarian => false,
-        :menu => nil
-      ),
-      Dish.create!(
-        :name => "Name",
-        :description => "MyText",
-        :category => "Category",
-        :category_display => "Category Display",
-        :portion_size => "Portion Size",
-        :spice => 1,
-        :hot => false,
-        :gluten_free => false,
-        :vegetarian => false,
-        :menu => nil
-      )
-    ])
+    assign(:dishes, dishes)
+    render
   end
 
-  it "renders a list of dishes" do
-    render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Category".to_s, :count => 2
-    assert_select "tr>td", :text => "Category Display".to_s, :count => 2
-    assert_select "tr>td", :text => "Portion Size".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+  it 'renders a title' do
+    expect(rendered).to have_css('h1', text: 'Listing dishes')
+  end
+
+  it 'renders a table header' do
+    expect(rendered).to have_css('thead th', text: 'Name')
+    expect(rendered).to have_css('thead th', text: 'Category')
+    expect(rendered).to have_css('thead th', text: 'Menu')
+    expect(rendered).to have_css('thead th', text: 'Restaurant')
+    expect(rendered).to have_css('thead th', text: 'Hidden')
+  end
+
+  it 'renders a table with dishes' do
+    expect(rendered).to have_css('tbody td', text: 'Asado')
+    expect(rendered).to have_css('tbody td', text: dish1.category.name)
+    expect(rendered).to have_css('tbody td', text: dish1.menu.name)
+    expect(rendered).to have_css('tbody td', text: 'Parrillada', count: 2)
+    expect(rendered).to have_link('Show', href: dish_path(dish1))
+    expect(rendered).to have_link('Edit', href: edit_dish_path(dish1))
+    expect(rendered).to have_link('Destroy', href: dish_path(dish1))
+    expect(rendered).to have_css('tbody td', text: 'Asado')
+    expect(rendered).to have_css('tbody td', text: dish2.category.name)
+    expect(rendered).to have_css('tbody td', text: dish2.menu.name)
+    expect(rendered).to have_link('Show', href: dish_path(dish2))
+    expect(rendered).to have_link('Edit', href: edit_dish_path(dish2))
+    expect(rendered).to have_link('Destroy', href: dish_path(dish2))
   end
 end
