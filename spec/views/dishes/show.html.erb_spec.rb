c2e4abb6a664d4d6b8f225c44d0f4e09083fec57
Braulio Martinez <braulio@paragon-labs.com>
2015-06-22 12:50:45 -0300

    Fix spec/views/dishes/show.html.erb_spec.rb

--- spec/views/dishes/show.html.erb_spec.rb
+++ spec/views/dishes/show.html.erb_spec.rb
@@ -1,32 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe "dishes/show", :type => :view do
-  before(:each) do
-    @dish = assign(:dish, Dish.create!(
-      :name => "Name",
-      :description => "MyText",
-      :category => "Category",
-      :category_display => "Category Display",
-      :portion_size => "Portion Size",
-      :spice => 1,
-      :hot => false,
-      :gluten_free => false,
-      :vegetarian => false,
-      :menu => nil
-    ))
+RSpec.describe 'dishes/show', :type => :view do
+  let(:ingredient) { FactoryGirl.create(:ingredient) }
+  let(:image) { FactoryGirl.create(:image, :with_avatar) }
+  let(:dish) do
+    FactoryGirl.create(
+      :dish,
+      :with_avatar,
+      name: 'Name',
+      description: 'My Description',
+      category: FactoryGirl.create(:category, name: 'My Category'),
+      portion_size: 'Portion Size',
+      spice: 1,
+      hot: false,
+      gluten_free: true,
+      vegetarian: true,
+      ingredients: [ingredient],
+    )
   end
 
-  it "renders attributes in <p>" do
+  before(:each) do
+    assign(:dish, dish)
+    assign(:images, [image])
+    allow(view).to receive(:current_user).and_return(nil)
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/Category/)
-    expect(rendered).to match(/Category Display/)
-    expect(rendered).to match(/Portion Size/)
-    expect(rendered).to match(/1/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(//)
+  end
+
+  it 'renders attributes in <p>' do
+    expect(rendered).to have_css('p', text: 'Name')
+    expect(rendered).to have_css('p', text: 'My Description')
+    expect(rendered).to have_css('p', text: 'Served Cold')
+    expect(rendered).to have_css('p', text: 'Gluten Free')
+    expect(rendered).to have_css('p', text: 'Vegetarian Dish')
+    expect(rendered).to have_css('p', text: 'Size:')
+    expect(rendered).to have_css('p', text: 'Portion Size')
+    expect(rendered).to have_css('p', text: 'Spice:')
+    expect(rendered).to have_css('p', text: '1/5')
+  end
+
+  it 'renders the ingredients' do
+    expect(rendered).to have_css('thead th', text: 'Ingredients')
+    expect(rendered).to have_css('tbody td', text: ingredient.name)
+  end
+
+  it 'renders the images' do
+    expect(rendered).to have_css("img[src*='#{dish.avatar.url(:medium)}']")
+    expect(rendered).to have_css("img[src*='#{image.avatar.url(:medium)}']")
+  end
+
+  it 'renders a link to go back to the restaurant' do
+    expect(rendered).to have_link('Back to Restaurant', href: restaurant_path(dish.restaurant))
+  end
+
+  context 'with admin' do
+    before do
+      assign(:dish, dish)
+      assign(:images, [image])
+      assign(:image, Image.new)
+      allow(view).to receive(:current_user).and_return(FactoryGirl.create(:admin))
+      render
+    end
+
+    it 'renders the admin panel' do
+      expect(rendered).to have_css('h3', text: 'Administrator Panel')
+      expect(rendered).to have_link('Edit Dish Details', href: edit_dish_path(dish))
+      expect(rendered).to have_css('h3', text: 'Dish Images')
+      expect(rendered).to have_css("img[src*='#{image.avatar.url(:thumb)}']")
+      expect(rendered).to have_link('Edit Image', href: edit_image_path(image))
+      expect(rendered).to have_css('h5', text: 'Add Additional Images')
+      expect(rendered).to render_template('images/_form')
+    end
   end
 end
