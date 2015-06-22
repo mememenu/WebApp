2dc6f7387072a2b47ebc34acd48d0ea80e900a52
Braulio Martinez <braulio@paragon-labs.com>
2015-06-22 11:03:13 -0300

    Fix spec/views/cuisines/edit.html.erb_spec.rb

--- spec/views/cuisines/edit.html.erb_spec.rb
+++ spec/views/cuisines/edit.html.erb_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "cuisines/edit", :type => :view do
+RSpec.describe 'cuisines/edit', :type => :view do
+  let(:cuisine) { Cuisine.create!(genre: 'French') }
+
   before(:each) do
-    @cuisine = assign(:cuisine, Cuisine.create!(
-      :type => ""
-    ))
+    assign(:cuisine, cuisine)
+    render
   end
 
-  it "renders the edit cuisine form" do
-    render
+  it 'has a title' do
+    expect(rendered).to have_css('h1', text: 'Editing cuisine')
 
-    assert_select "form[action=?][method=?]", cuisine_path(@cuisine), "post" do
+  end
 
-      assert_select "input#cuisine_type[name=?]", "cuisine[type]"
-    end
+  it 'renders the edit cuisine form' do
+    expect(rendered).to render_template('cuisines/_form')
+  end
+
+  it 'has links' do
+    expect(rendered).to have_link('Show', href: cuisine_path(cuisine))
+    expect(rendered).to have_link('Back', href: cuisines_path)
   end
 end
