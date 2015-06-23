ae5cec36d8cd4a49063db8c278b1bce03c36a79b
Braulio Martinez <braulio@paragon-labs.com>
2015-06-23 15:01:02 -0300

    Add feature specs for the restaurants.

--- spec/factories/restaurant_factory.rb
+++ spec/factories/restaurant_factory.rb
@@ -12,5 +12,14 @@ FactoryGirl.define do
     trait :with_avatar do
       avatar { fixture_file_upload(Rails.root + 'spec/fixtures/images/896x1052.jpeg', 'image/jpeg') }
     end
+
+    factory :restaurant_with_menus_and_categories do
+      after(:create) do |restaurant, _|
+        create(:menu, restaurant: restaurant, name: 'Test Menu 1')
+        create(:menu, restaurant: restaurant, name: 'Test Menu 2')
+        create(:category, restaurant: restaurant, name: 'Test Category 1')
+        create(:category, restaurant: restaurant, name: 'Test Category 2')
+      end
+    end
   end
 end
