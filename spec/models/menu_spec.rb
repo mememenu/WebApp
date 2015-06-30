4a231db2c34ca85f791df6f3c4038f1d857bc0c4
Braulio Martinez <braulio@paragon-labs.com>
2015-06-30 14:51:13 -0300

    Refactor model menu spec

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,67 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Menu, :type => :model do
-  before :each do
-    @restaurant = Restaurant.create(name: "Mmmm", address_1: "123 fake lane", address_2: "apartment 4", city: "Miami", state: "FL", zipcode: "33132", phone: "1234567890", description: "tasty food", dollars: 2, reservations: true, zone: "Miami", slug: "Mmmm", foursquare_id: "foursquare_id")
-  end
-  
-  def create_menu
-    @menu = Menu.new
-    @menu.name = "Lunch"
-    @menu.restaurant_id = @restaurant.id
-    @menu.priority = 1
-    @menu.save
-    expect(Menu.count).to eq(1)
+RSpec.describe Menu, type: :model do
+  let(:menu) { FactoryGirl.create(:menu) }
+  let(:restaurant) { menu.restaurant }
+
+  it "becomes hidden when corresponding restaurant becomes hidden" do
+    menu.update_attributes(hide: false)
+    restaurant.update_attributes(hide: true)
+
+    expect(menu.reload.hide).to be_truthy
   end
 
-  it "should save successfully when all information present" do
-    expect(Menu.count).to eq(0)
-    create_menu
-    expect(Menu.count).to eq(1)
+  it "becomes unhidden if restaurant becomes unhidden" do
+    menu.update_attributes(hide: true)
+    restaurant.update_attributes(hide: false)
+
+    expect(menu.reload.hide).to be_falsy
   end
 
-  it "should become hidden when corresponding restaurant becomes hidden" do 
-    create_menu
-
-    @restaurant.hide = true
-    @restaurant.save
-
-    @restaurant.reload
-    @menu.reload
-
-    expect(@menu.hide).to eq(@restaurant.hide)
-  end
-
-
-  it "should become unhidden if restaurant becomes unhidden" do
-    create_menu
-
-    @restaurant.hide = true
-    @restaurant.save
-
-    @restaurant.reload
-    @menu.reload
-
-    expect(@menu.hide).to eq(@restaurant.hide)
-
-    @restaurant.hide = false
-    @restaurant.save
-
-    @restaurant.reload
-    @menu.reload
-
-    expect(@menu.hide).to eq(@restaurant.hide)
-  end
-
-  it "should be destroyed when corresponding restaurant is destroyed" do
-    create_menu
-
-    @restaurant.destroy
+  it "gets destroyed when corresponding restaurant is destroyed" do
+    restaurant.destroy
 
     expect(Restaurant.count).to eq(0)
     expect(Menu.count).to eq(0)
   end
-
-
-
 end
