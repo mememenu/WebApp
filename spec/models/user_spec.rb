1a842e2d9bd29c25c524b99057a2272f982daa79
Braulio Martinez <braulio@paragon-labs.com>
2015-06-30 14:51:28 -0300

    Refactor model user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,20 @@
 require 'rails_helper'
 
-  def new_user
-    user = User.new
-    user.email = "alfonsopintos@gmail.com"
-    user.password = "12345678"
-    user.password_confirmation = "12345678"
-    user.save
+RSpec.describe User, type: :model do
+  it "does not save a user if the password is less that 8 characters" do
+    user = FactoryGirl.build(:user, password: '123')
+    expect(user).not_to be_valid
   end
 
-RSpec.describe User, :type => :model do
+  it "does not save a user if the email is already taken" do
+    user = FactoryGirl.create(:user)
+    duplicated_user = FactoryGirl.build(:user, email: user.email)
 
-  it "should save a user upon successful registration" do 
-    expect(User.count).to eq(0)
-    new_user
-    expect(User.count).to eq(1)
+    expect(duplicated_user).not_to be_valid
   end
 
-  it "should not save a user if the password is less that 8 characters" do 
-    expect(User.count).to eq(0)
-    user = User.new
-    user.email = "alfonsopintos@gmail.com"
-    user.password = "1234567"
-    user.password_confirmation = "1234567"
-    user.save
-    expect(User.count).to eq(0)
+  it "does not save a user if the email is not valid" do
+    user = FactoryGirl.build(:user, email: "alfonsopintosgmail.com")
+    expect(user).not_to be_valid
   end
-
-    it "should not save a user if the email is already taken" do 
-    expect(User.count).to eq(0)
-    new_user
-    expect(User.count).to eq(1)
-    duplicate_user = User.new
-    duplicate_user.email = "alfonsopintos@gmail.com"
-    duplicate_user.password = "12345678"
-    duplicate_user.password_confirmation = "12345678"
-    duplicate_user.save
-    expect(User.count).to eq(1)
-  end
-
-  it "should not save a user if the email is not valid" do 
-    expect(User.count).to eq(0)
-    user = User.new
-    user.email = "alfonsopintosgmail.com"
-    user.password = "12345678"
-    user.password_confirmation = "12345678"
-    user.save
-    expect(User.count).to eq(0)
-  end    
-
 end
