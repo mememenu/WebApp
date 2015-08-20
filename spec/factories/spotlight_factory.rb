a938913e66c84ab782cd82f8def31c368f080f81
Braulio Martinez <braulio@paragon-labs.com>
2015-08-20 18:25:11 -0300

    Remove Events from demo data and factories for now

--- spec/factories/spotlight_factory.rb
+++ spec/factories/spotlight_factory.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :spotlight do
     trait :with_items do
       after :build do |sp|
-        2.times do
-          ['list', 'place', 'event'].each do |item_type|
+        3.times do
+          ['list', 'place'].each do |item_type|
             sp.spotlight_items << FactoryGirl.create(:"#{item_type}_item", :with_image)
           end
         end
