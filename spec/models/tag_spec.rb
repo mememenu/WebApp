c4728ef173b10e5834e0fb28cf97f732aeea5002
Braulio Martinez <braulio@paragon-labs.com>
2015-08-28 20:12:34 -0300

    Add validations

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Tag, type: :model do
+  it { should validate_presence_of(:name) }
+
+  context "name uniqueness" do
+    it "is invalid if there is other one with the same name" do
+      tag = FactoryGirl.create(:tag)
+
+      expect(FactoryGirl.build(:tag, name: tag.name)).not_to be_valid
+    end
+
+    it "is valid if there is no other with the same name" do
+      expect(FactoryGirl.build(:tag, name: "Test tag")).to be_valid
+    end
+  end
+end
