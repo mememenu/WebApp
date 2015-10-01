47a53e3e4a92a8f112c95ad0e6166cd0c41f13b9
Braulio Martinez <braulio@paragon-labs.com>
2015-10-01 13:43:16 -0300

    Remove deprecated spec

--- spec/requests/api/places_spec.rb
+++ spec/requests/api/places_spec.rb
@@ -6,17 +6,6 @@ RSpec.describe 'Places API', type: :request do
     FactoryGirl.create_list(:place, 2, region: 'NOLA', hide: false)
   end
 
-  describe '#place_info' do
-    it 'returns a list of places that are not hidden' do
-      FactoryGirl.create(:place, hide: true)
-      get '/ios/restaurant_info', format: :json
-
-      expect(response).to be_success
-      expect(json.length).to eq(4)
-      expect(response).to render_template('ios/place_info')
-    end
-  end
-
   describe '#miami_places_list' do
     it 'returns a list of places in Miami that are not hidden' do
       FactoryGirl.create(:place, hide: true, region: 'MIA')
