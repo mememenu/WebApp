eb2dc06f7e4e6b6afe7871d8b20d2e2cbb2619e1
Braulio Martinez <braulio@paragon-labs.com>
2015-10-01 13:13:19 -0300

    Additions to place show api v1

--- app/serializers/api/v1/place/show_serializer.rb
+++ app/serializers/api/v1/place/show_serializer.rb
@@ -1,6 +1,7 @@
 class Api::V1::Place::ShowSerializer < Api::V1::PlaceSerializer
   attributes :header, :formatted_phone, :location, :full_address, :price,
-             :google_id, :foursquare_id, :quotes
+             :google_id, :foursquare_id, :quotes, :phone, :website, :facebook,
+             :twitter, :instagram
 
   has_many :menus, serializer: Api::V1::MenuSerializer
   has_one :header
