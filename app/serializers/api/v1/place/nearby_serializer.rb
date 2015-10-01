e09f9eaa73aa258c2ab9ae63855720c474433cc6
Braulio Martinez <braulio@paragon-labs.com>
2015-10-01 13:16:54 -0300

    Add status to both nearby endpoints

--- app/serializers/api/v1/place/nearby_serializer.rb
+++ app/serializers/api/v1/place/nearby_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Place::NearbySerializer < Api::V1::PlaceSerializer
-  attributes :distance
+  attributes :distance, :status
 
   def distance
     object.distance_to(serialization_options[:sent_location])
