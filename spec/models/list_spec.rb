5c9408daa74b72749b31f84d24257f2f096ccbe9
Braulio Martinez <braulio@paragon-labs.com>
2015-10-27 15:01:56 -0300

    Migrate spec to new logic

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,12 +3,6 @@ require 'rails_helper'
 describe List, type: :model do
   it { should validate_presence_of(:name) }
 
-  it "sets the default name if user is present" do
-    list = FactoryGirl.create(:user_list)
-
-    expect(list.name).to eq("My List")
-  end
-
   describe 'validations' do
     it "validates that avatar is present" do
       list = FactoryGirl.build(:generic_list, avatar: nil)
