ca9b8c60338d89d4a383a7859c0833e04bf46e7d
Braulio Martinez <braulio@paragon-labs.com>
2015-10-28 15:33:52 -0300

    Post auth tweaks

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::BaseController < ActionController::Base
   doorkeeper_for :all
 
   respond_to :json
+
+  def current_user
+    super || User.find_by_id(doorkeeper_token[:resource_owner_id])
+  end
 end
